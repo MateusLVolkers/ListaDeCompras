bd6ee2e8f27b920e8615ea961f431858cb119412
Mateus Loureiro Volkers <mateuslvolkers@gmail.com>
2023-11-27 10:36:34 -0300

    Criado o layout do formulário de cadastro, realizado alguns ajustes nos layout

--- app/src/main/java/com/mateuslvolkers/listadecompras/ui/MainActivity.kt
+++ app/src/main/java/com/mateuslvolkers/listadecompras/ui/MainActivity.kt
@@ -2,11 +2,33 @@ package com.mateuslvolkers.listadecompras.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.mateuslvolkers.listadecompras.R
+import com.mateuslvolkers.listadecompras.model.Produto
+import com.mateuslvolkers.listadecompras.ui.recyclerview.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
+        recyclerview.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
+            Produto(nome = "produto 1",
+                    descricao = "Descrição do produto 1",
+                    valor = BigDecimal("19.99")
+            ),
+            Produto(nome = "mange",
+                    descricao = "verde",
+                    valor = BigDecimal("15.00")
+            ),
+            Produto(nome = "teste",
+                descricao = "testando",
+                valor = BigDecimal("1.00")
+            ),
+        ))
+
+
     }
 }
